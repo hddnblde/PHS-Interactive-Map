1afbfb025f44d1b26ba193e3df87f0178ccf7960
John Carlo Gamboa <jc.gamboa@dgxhub.com>
2017-12-06 17:31:01 +0800

    Moved navigation-related scripts to a folder.

---
+++ Assets/Scripts/Objects/Schedule/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scheduling
+{
+	public struct TimeFormat
+	{
+		[SerializeField]
+		private int hour;
+
+		[SerializeField]
+		private int minute;
+	}
+}
