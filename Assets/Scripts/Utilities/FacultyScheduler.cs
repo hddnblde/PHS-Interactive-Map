a2fda6e084d96673a5c2df35f5ec3bb5598d517c
John Carlo Gamboa <jc.gamboa@dgxhub.com>
2017-12-22 16:53:17 +0800

    Added object picker for faculty scheduler tool. This will be used for assigning room and section cluster to teacher's schedule.

--- Assets/Scripts/Utilities/FacultyScheduler.cs
+++ Assets/Scripts/Utilities/FacultyScheduler.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Faculty.Schedules;
+using Students;
+using Map;
 
 public class FacultyScheduler : EditorWindow
 {
@@ -15,9 +18,32 @@ public class FacultyScheduler : EditorWindow
 		scheduler.Show();
 	}
 
+	#region Data Structure
+	private struct SelectedItem
+	{
+		public int column;
+		public int row;
+
+		public bool NoneSelected()
+		{
+			return column == -1 && row == -1;
+		}
+	}
+
+	private enum SelectionDepth
+	{
+		SelectRoom,
+		SelectSectionCluster
+	}
+	#endregion
+	private SelectedItem currentSelectedItem;
+	private SelectionDepth selectionDepth = SelectionDepth.SelectRoom;
+	private bool objectPickerWindowOpen = false;
+
 	private void OnGUI()
 	{
 		DrawTable(position.width, position.height, 3);
+		HandleScheduling();
 	}
 
 	private void DrawTable(float width, float height, int period)
@@ -46,14 +72,69 @@ public class FacultyScheduler : EditorWindow
 		float width = (rect.width / columns) - (spacing * 2);
 		float height = (rect.height / rows) - (spacing * 2);
 		Rect boxRect = new Rect(rect.position, new Vector2(width, height));
+
+		Color defaultGUIColor = GUI.color;
+
 		for(int column = 0; column < columns; column++)
 		{
 			boxRect.x = rect.position.x + ((spacing * 1.5f) * (column + 1)) + (width * column);
 			for(int row = 0; row < rows; row++)
 			{
 				boxRect.y = rect.position.y + ((spacing * 1.5f) * (row + 1)) + (height * row);
+
+				Room selectedRoom = null;
+
+				GUI.color = Color.clear;
+				if(!currentSelectedItem.NoneSelected() && GUI.Button(boxRect, ""))
+				{
+					currentSelectedItem.column = column;
+					currentSelectedItem.row = row;
+					objectPickerWindowOpen = true;
+				}
+				GUI.color = defaultGUIColor;
+
 				GUI.Box(boxRect, "");
 			}
+		}
+	}
+
+	private void HandleScheduling()
+	{
+		if(currentSelectedItem.NoneSelected())
+			return;
+
+		Room selectedRoom = null;
+		SectionCluster selectedSectionCluster = null;
+
+		if(!objectPickerWindowOpen)
+		{
+			int controlID = EditorGUIUtility.GetControlID(FocusType.Passive);
+
+			if(selectionDepth == SelectionDepth.SelectRoom)
+				EditorGUIUtility.ShowObjectPicker<Room>(selectedRoom, false, "", controlID);
+			else
+				EditorGUIUtility.ShowObjectPicker<SectionCluster>(selectedSectionCluster, false, "", controlID);
+
+			objectPickerWindowOpen = true;
+		}
+
+		string commandName = Event.current.commandName;
+
+		if(commandName == "ObjectSelectorClosed")
+		{
+			if(selectionDepth == SelectionDepth.SelectRoom)
+				selectedRoom = (Room)EditorGUIUtility.GetObjectPickerObject();
+			else
+			{
+				selectedSectionCluster = (SectionCluster)EditorGUIUtility.GetObjectPickerObject();
+				currentSelectedItem.column = -1;
+				currentSelectedItem.row = -1;
+
+				Debug.Log("SELECTED ROOM : " + selectedRoom.name);
+				Debug.Log("SELECTED SECTION : " + selectedSectionCluster.name);
+			}
+
+			objectPickerWindowOpen = false;
 		}
 	}
 }
